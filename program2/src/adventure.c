489f6b71a5cf8ea8a223b029d8b8344dd982b8b7
Timo Nicolai <timonicolai@arcor.de>
2019-03-16 11:34:51 +0100

    program2: improve main function resource cleanup

--- program2/src/adventure.c
+++ program2/src/adventure.c
@@ -12,34 +12,45 @@ char *progname;
 int main(int argc, char **argv) {
     progname = basename(argv[0]);
 
+    char *room_dir = NULL, **room_names = NULL;
+    struct room *rooms = NULL;
+
+    int room_idx, room_name_idx, ret = 1;
+
     /* create room directory */
-    char *room_dir;
     if (create_room_dir(&room_dir) == -1)
-        return -1;
+        return 1;
 
     /* parse room names */
-    char **room_names;
     int num_room_names = parse_room_names(&room_names);
     if (num_room_names == -1)
-        return -1;
+        goto cleanup1;
 
     /* create rooms */
     if (create_rooms(room_dir, room_names, num_room_names) == -1)
-        return -1;
+        goto cleanup2;
 
     /* read rooms in again */
-    struct room *rooms;
-    if (read_rooms(room_dir, &rooms) == -1)
-        return -1;
+    int num_rooms;
+    if ((num_rooms = read_rooms(room_dir, &rooms)) == -1)
+        goto cleanup3;
 
-    /* free resources */
-    free(room_dir);
+    ret = 0;
 
-    int room_name_idx = 0;
+cleanup3:
+    for (room_idx = 0; room_idx < num_rooms; ++room_idx)
+        free(rooms[room_idx].name);
+
+    free(rooms);
+
+cleanup2:
     for (room_name_idx = 0; room_name_idx < num_room_names; ++room_name_idx)
         free(room_names[room_name_idx]);
 
     free(room_names);
 
-    return 0;
+cleanup1:
+    free(room_dir);
+
+    return ret;
 }
