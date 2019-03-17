3e39ddb947515598f81d939573d7209710b8a3c5
Timo Nicolai <timonicolai@arcor.de>
2019-03-17 13:35:34 +0100

    program4: add socket debug output and fix receive bug

--- program4/src/socket.c
+++ program4/src/socket.c
@@ -10,6 +10,43 @@
 #include "util.h"
 
 
+#ifndef NDEBUG
+#include <stdio.h>
+
+static void print_long_hex(long val) {
+    static char buf[sizeof(val)];
+
+    int x, i;
+
+    memcpy(buf, &val, sizeof(val));
+
+    printf("0x");
+
+    x = 1;
+    if (((char *) &x)[0]) {
+        for (i = sizeof(val) - 1; i >= 0; --i)
+            printf("%02x", buf[i]);
+    } else {
+        for (i = 0; i < (int) sizeof(val); ++i)
+            printf("%02x", buf[i]);
+    }
+}
+
+
+static void print_block_preview(char *block, long block_length) {
+    int i;
+
+    for (i = 0; i < 10; ++i)
+        printf("%02x", block[i]);
+
+    printf("...");
+
+    for (i = 9; i >= 0; --i)
+        printf("%02x", block[block_length - i - 1]);
+}
+#endif
+
+
 int create_socket(int port, enum socket_mode mode) {
     int sock_fd;
     struct sockaddr_in addr;
@@ -50,12 +87,23 @@ int send_block(int sock_fd, char *block, long block_length) {
     long block_offs = 0, chunk_size;
 
     /* send block size */
+#ifndef NDEBUG
+    printf("sending block size: ");
+    print_long_hex(block_length);
+    printf(" (%ld)\n", block_length);
+#endif
+
     if (write(sock_fd, &block_length, sizeof(block_length)) == -1) {
         errprintf("failed to send block size (%s)", strerror(errno));
         return -1;
     }
 
     /* send block data */
+#ifndef NDEBUG
+    printf("sending block: ");
+    print_block_preview(block, block_length);
+#endif
+
     while (block_offs < block_length) {
         chunk_size = CHUNK_SIZE;
         if (block_offs + chunk_size > block_length)
@@ -71,6 +119,32 @@ int send_block(int sock_fd, char *block, long block_length) {
         block_offs += write_size;
     }
 
+#ifndef NDEBUG
+    printf(" (%ld bytes)\n", block_offs);
+#endif
+
+    return 0;
+}
+
+
+static int receive_block_length(int sock_fd, long *block_length) {
+    char buf[sizeof(*block_length)];
+    ssize_t read_size;
+    size_t read_offs = 0;
+
+    while (read_offs < sizeof(buf)) {
+        read_size = read(sock_fd, buf + read_offs, sizeof(buf) - read_offs);
+
+        if (read_size == -1) {
+            errprintf("failed to receive block size (%s)", strerror(errno));
+            return -1;
+        }
+
+        read_offs += read_size;
+    }
+
+    memcpy(block_length, buf, sizeof(buf));
+
     return 0;
 }
 
@@ -78,15 +152,21 @@ int send_block(int sock_fd, char *block, long block_length) {
 int receive_block(int sock_fd, char **block, long *block_length) {
     char buf[BUF_SIZE];
     ssize_t read_size;
-    long block_offs = 0;
+    long block_offs = 0, chunk_size;
 
     /* receive block size */
-    if (read(sock_fd, buf, sizeof(*block_length)) == -1) {
+    if (receive_block_length(sock_fd, block_length) == -1) {
         errprintf("failed to receive block size (%s)", strerror(errno));
         return -1;
     }
 
     memcpy(block_length, buf, sizeof(*block_length));
+
+#ifndef NDEBUG
+    printf("received block size: ");
+    print_long_hex(*block_length);
+    printf(" (%ld)\n", *block_length);
+#endif
 
     /* allocate block */
     *block = malloc(*block_length);
@@ -97,7 +177,11 @@ int receive_block(int sock_fd, char **block, long *block_length) {
 
     /* receive block data */
     while (block_offs < *block_length) {
-        if ((read_size = read(sock_fd, buf, BUF_SIZE)) == -1) {
+        chunk_size = BUF_SIZE;
+        if (block_offs + chunk_size > *block_length)
+            chunk_size = *block_length - block_offs;
+
+        if ((read_size = read(sock_fd, buf, chunk_size)) == -1) {
             errprintf("failed to receive data (%s)", strerror(errno));
             free(block);
             return -1;
@@ -110,6 +194,12 @@ int receive_block(int sock_fd, char **block, long *block_length) {
 
         block_offs += read_size;
     }
+
+#ifndef NDEBUG
+    printf("received block: ");
+    print_block_preview(*block, *block_length);
+    printf(" (%ld bytes)\n", block_offs);
+#endif
 
     return 0;
 }
