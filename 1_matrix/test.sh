600477d4145cdb74dd07e5c870c16cd775aafd41
Timo Nicolai <timonicolai@arcor.de>
2019-03-17 19:28:27 +0100

    program1: add status output to test.sh

--- 1_matrix/test.sh
+++ 1_matrix/test.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
+echo "dims..."
 ./matrix dims matrix1.txt > /dev/null
 [ $? -ne 0 ] && exit 1
 
+echo "transpose..."
 ./matrix transpose matrix1.txt > /dev/null
 [ $? -ne 0 ] && exit 1
 
+echo "mean..."
 ./matrix mean matrix1.txt > /dev/null
 [ $? -ne 0 ] && exit 1
 
+echo "add..."
 ./matrix add matrix1.txt matrix2.txt > /dev/null
 [ $? -ne 0 ] && exit 1
 
+echo "multiply..."
 ./matrix multiply matrix1.txt matrix2.txt > /dev/null
 [ $? -ne 0 ] && exit 1
 
